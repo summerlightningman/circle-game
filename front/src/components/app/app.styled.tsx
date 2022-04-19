a225bd6c7c49937ca2df8362ca1299edec7dd16b
summerlightningman <div8@bk.ru>
2022-04-19 15:24:30 +0300

    added styles for App component

---
+++ front/src/components/app/app.styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const AppStyled = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+export default AppStyled
